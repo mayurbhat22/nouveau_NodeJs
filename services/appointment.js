e26850132aac6807b8b8340c1e68c284533dc1e1
Evan Jackson <evanjack@iu.edu>
2023-11-08 19:56:44 -0500

    start on appointment booking

---
+++ services/appointment.js
@@ -0,0 +1,36 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+
+async function getAllAppointmentsAfter(day) {
+
+}
+
+
+async function getUnavailableDaysAfter(day) {
+
+}
+
+async function getAllAppointmentsOn(day) {
+
+}
+
+async function getAllAppointmentsBeforeByDoctor(did, day) {
+
+}
+
+async function getAllAppointmentsAfterByDoctor(did, day) {
+
+}
+
+async function getAllAppointmentsBeforeByPatient(pid, day) {
+
+}
+
+async function getAllAppointmentsAfterByPatient(pid, day) {
+
+}
+
+async function createAppointmentOn(day, pid, did, symptoms) {
+
+}
