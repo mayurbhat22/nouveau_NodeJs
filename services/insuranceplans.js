bdd13ece128c347481c82c1936b07cf305b6409d
Mayur Bhat <mayappbhat@gmail.com>
2023-11-30 22:50:08 -0500

    Commit

--- services/insuranceplans.js
+++ services/insuranceplans.js
@@ -131,4 +131,3 @@ module.exports = {
   getPlanByPatient,
   getPlanByPatientIdWithProviderInfo,
 };
-//testing something
---
+++ services/user.js
@@ -0,0 +1,16 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+async function getUserById(userid) {
+  const user = await prisma.user.findFirst({
+    where: {
+      userid: userid,
+    },
+  });
+
+  return user;
+}
+
+module.exports = {
+  getUserById,
+};
