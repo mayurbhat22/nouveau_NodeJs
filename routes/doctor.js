9208676ff7e8a2962838414dd1486228daf11fa5
Evan Jackson <evanjack@iu.edu>
2023-12-01 10:54:25 -0500

    Mostly complete feedback backend

--- routes/doctor.js
+++ routes/doctor.js
@@ -3,9 +3,13 @@ const router = express.Router();
 
 const {
     //doctorPatientsPostController,
-    doctorUpcomingApptsPostController
+    doctorUpcomingApptsPostController,
+    doctorGetDetailsController,
+    doctorLeaveFeedbackPostController
 } = require("../controllers/doctor");
 
 //router.post("/patients", doctorPatientsPostController);
 router.post("/upcoming", doctorUpcomingApptsPostController);
+router.post("/details", doctorGetDetailsController);
+router.post("/feedback", doctorLeaveFeedbackPostController)
 module.exports = router;
