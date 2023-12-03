d4547c84cac69f29dd5d2920458ee572470b4980
Mayur Bhat <mayappbhat@gmail.com>
2023-12-02 21:31:17 -0500

    Google Auth Changes

--- routes/login.js
+++ routes/login.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    loginPostController,
-    loginDuoAuthStatusPostController,
-    loginDuoAuthPostController
+  loginPostController,
+  loginDuoAuthStatusPostController,
+  loginDuoAuthPostController,
+  loginGoogleAuth,
 } = require("../controllers/login");
 
 router.post("/", loginPostController);
 router.post("/auth", loginDuoAuthPostController);
 router.post("/auth/status", loginDuoAuthStatusPostController);
+router.post("/googleauth", loginGoogleAuth);
+
 module.exports = router;
