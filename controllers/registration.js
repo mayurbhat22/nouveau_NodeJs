fb1ba59e015a228d83bf0f4507284e95dddf5c06
Mayur Bhat <mayappbhat@gmail.com>
2023-12-08 21:14:32 -0500

    Fixed the google auth bug

--- controllers/registration.js
+++ controllers/registration.js
@@ -2,17 +2,33 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const bcrypt = require("bcrypt");
 const duo = require("../services/duo_interface");
-const doctordb = require("../services/doctor")
+const doctordb = require("../services/doctor");
+const { UserRefreshClient } = require("google-auth-library");
 const salt = 10;
 
 // Determine if there is already a user for the entered email
 // path variable email - the email to be checked
 // return - json message with boolean attribute unique - it is true if there is no account for the entered email
+// const registrationGetContoller = async (req, res) => {
+//   const user = await prisma.user.findFirst({
+//     where: { email: req.params["email"] },
+//   });
+//   res.json({ unique: user == null });
+// };
+
 const registrationGetContoller = async (req, res) => {
-  const user = await prisma.user.findFirst({
-    where: { email: req.params["email"] },
-  });
+  const users = await prisma.user.findMany({});
+
+  let user = null;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].email === req.params["email"]) {
+      user = users[i];
+    }
+  }
+
   res.json({ unique: user == null });
+
+  console.log(user);
 };
 
 // Attempt to sign user up - Check if the email exists and that the password is correct
@@ -33,7 +49,6 @@ const registrationPostContoller = async (req, res) => {
     },
   });
 
-
   /*
 
   Doctors will be added to doctor table once they update their profile - keeps them from showing up in searches until they have valid info
@@ -43,8 +58,7 @@ const registrationPostContoller = async (req, res) => {
   }
   */
 
-
-  res.json({userid: user.userid});
+  res.json({ userid: user.userid });
 };
 
 /* 
@@ -70,7 +84,6 @@ const registrationDuoCheckGetController = async (req, res) => {
 }
 */
 
-
 // Attempt to enroll user in duo
 // request body - userid of the user to enroll, used as username for user in duo
 // response body - returns the response from the duo api call, including the url for the barcode, the activation code, and the user's duo id
@@ -78,20 +91,19 @@ const registrationDuoCheckGetController = async (req, res) => {
 const registrationDuoEnrollPostController = async (req, res) => {
   const body = req.body;
 
-  const callback = (body, statusCode, statusMessage, error=false) => {
+  const callback = (body, statusCode, statusMessage, error = false) => {
     res.statusCode = statusCode;
     res.statusMessage = statusMessage;
-    if(!error) {
-      res.json(body)
-      res.send()
+    if (!error) {
+      res.json(body);
+      res.send();
     } else {
-      res.send(body)
+      res.send(body);
     }
-  }
+  };
 
-  duo.enroll(body.userid, callback)
-}
-
+  duo.enroll(body.userid, callback);
+};
 
 // Check status of user's enrollment in duo
 // request body - user_id DUO id (NOT the one in the db, returned when enroll is initially called) of the user being checked, activation_code the activation code from the enroll call
@@ -99,52 +111,53 @@ const registrationDuoEnrollPostController = async (req, res) => {
 const registrationDuoEnrollStatusPostController = async (req, res) => {
   const body = req.body;
 
-  const callback = (body, statusCode, statusMessage, error=false) => {
+  const callback = (body, statusCode, statusMessage, error = false) => {
     res.statusCode = statusCode;
     res.statusMessage = statusMessage;
-    if(!error) {
-      res.json(body)
-      res.send()
+    if (!error) {
+      res.json(body);
+      res.send();
     } else {
-      res.send(body)
+      res.send(body);
     }
-  }
+  };
 
-  duo.enroll_status(body.user_id, body.activation_code, callback) 
-}
-
+  duo.enroll_status(body.user_id, body.activation_code, callback);
+};
 
 // for users who opt out of enrolling in mfa when registering, set their mfa status to in the db
 // request body - userid id of the user who we are disabling mfa for
 // response body - returns the response from the duo api call, just the status of the enrollment (success, invalid, or waiting)
 const registrationResetMFAController = async (req, res) => {
-  const body = req.body
-  const userid = typeof(body.userid) == 'string' ? parseInt(body.userid) : body.userid
+  const body = req.body;
+  const userid =
+    typeof body.userid == "string" ? parseInt(body.userid) : body.userid;
 
   // set up admin api and delete user as well
 
-  prisma.user.update({
-    where: {
-      userid: userid,
-    },
-    data: {
-      mfa: false,
-    },
-  }).then((user) => {
-    res.json({userid: parseInt(user.userid)})
-    res.send()
-  })
-  .catch((e) => {
-    res.statusCode = 400;
-    res.send("User not found");
-  });
-}
-
+  prisma.user
+    .update({
+      where: {
+        userid: userid,
+      },
+      data: {
+        mfa: false,
+      },
+    })
+    .then((user) => {
+      res.json({ userid: parseInt(user.userid) });
+      res.send();
+    })
+    .catch((e) => {
+      res.statusCode = 400;
+      res.send("User not found");
+    });
+};
 
 module.exports = {
   registrationGetContoller,
   registrationPostContoller,
   registrationDuoEnrollStatusPostController,
   registrationDuoEnrollPostController,
-  registrationResetMFAController
+  registrationResetMFAController,
 };
