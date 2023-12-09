243f69092298c7e8df38a5eeafb3dd6bb003ba53
Mayur Bhat <mayappbhat@gmail.com>
2023-12-09 11:05:03 -0500

    Fixing google auth bug

--- controllers/login.js
+++ controllers/login.js
@@ -11,7 +11,7 @@ const CLIENT_ID =
   "881661732927-mmbgpuotn8vha5s5basldolcel8ad2cv.apps.googleusercontent.com";
 const client = new OAuth2Client();
 // let REGISTRATION_URL = "http://localhost:8080/registration";
-let REGISTRATION_URL = "https://nouveauhealth.azurewebsites.net";
+let REGISTRATION_URL = "https://nouveau-app.azurewebsites.net/registration";
 // Attempt to log user in - Check if the email exists and that the password is correct
 // body - json with fields email, password, and role
 // return - if unsuccessful, appropriate error message, otherwise the id and mfa status of the user who just logged in
